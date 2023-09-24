00a444a030ad6e0028b5dae93237deb64d22c8e1
Petar <67274324+Petar1304@users.noreply.github.com>
2023-09-24 12:24:24 +0200

    Update MarketPlace.sol

--- MarketPlace.sol
+++ MarketPlace.sol
@@ -1,12 +1,12 @@
 // SPDX-License-Identifier: GPL-3.0
-pragma solidity ^0.8.4;
+pragma solidity ^0.8.7;
 
 import "@openzeppelin/contracts/token/ERC721/IERC721.sol";
 import "@openzeppelin/contracts/security/ReentrancyGuard.sol";
 
 contract Marketplace is ReentrancyGuard {
-    address payable public immutable feeAccount; //account koji prima feejeve
-    uint public immutable feePercent; //fee za prodaje
+    address payable public immutable feeAccount;
+    uint public immutable feePercent;
     uint public itemCount;
 
     struct Item {
@@ -51,10 +51,8 @@ contract Marketplace is ReentrancyGuard {
 
         itemCount++;
 
-        //prebaci nft od vlasnika na račun ovog contracta
         _nft.transferFrom(msg.sender, address(this), _tokenId);
 
-        //dodaj item u mapping
         items[itemCount] = Item(
             itemCount,
             _nft,
@@ -71,7 +69,6 @@ contract Marketplace is ReentrancyGuard {
 
         uint _totalPrice = getTotalPrice(_itemId);
 
-        //storage => varijabla se čita direktno iz mappinga, ne stvara se kopija
         Item storage item = items[_itemId]; 
         require(_itemId > 0 && _itemId <= itemCount, "item doesn't exist");
         require(
@@ -80,13 +77,11 @@ contract Marketplace is ReentrancyGuard {
         );
         require(!item.sold, "item already sold");
 
-        //plati prodavacu i fee accountu
         item.seller.transfer(item.price);
         feeAccount.transfer(_totalPrice - item.price);
 
         item.sold = true;
 
-        //prebaci NFT sa contracta na buyera
         item.nft.transferFrom(address(this), msg.sender, item.tokenId);
 
         emit Bought(
@@ -99,7 +94,6 @@ contract Marketplace is ReentrancyGuard {
         );
     }
 
-    //vraća cijenu itema + fees
     function getTotalPrice(uint _itemId) public view returns (uint) {
         return ((items[_itemId].price * (100 + feePercent)) / 100);
     }
